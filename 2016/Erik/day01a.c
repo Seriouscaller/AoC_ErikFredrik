74c1bee51d6733c319fccd1dd1bde6030bd8f055
Erik Karlsson <ralliartswe@gmail.com>
2026-09-22 20:14:47 +0200

    Psudocode and file handling complete

--- 2016/Erik/day01a.c
+++ 2016/Erik/day01a.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello");
+int read_file(char* file_name, char* array){
+    FILE *fp = fopen(file_name, "r");
+    char ch;
+    
+    if(fp == NULL) {
+        return 1;
+    }
+
+    int i = 0;
+    while ((ch = fgetc(fp)) != EOF){
+        array[i++] = ch;
+    }
+    array[++i] = '\0';
+    fclose(fp);
 
     return 0;
 }
+
+int main(){
+    char* file_name = "C:/Users/ralli/Documents/Code/Github/AoC_ErikFredrik/2016/Erik/day01_input_sample.txt";
+    char array[500];
+    
+    int ret = read_file(file_name, array);
+    if(ret == 1 ){
+        printf("Unable to open file!");
+        exit(1);
+    }
+
+    printf("%s", array);
+    
+    for(int i = 0; i < 20; i++){
+        printf("");
+    }
+    
+    return 0;
+}
+    
